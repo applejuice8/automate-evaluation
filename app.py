da86827583d3e54960c0ac6c11ffb998660a1720
Colin <138344363+applejuice8@users.noreply.github.com>
2025-10-01 08:45:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,89 @@
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-import time
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-class Scraper():
+class Evaluator():
     def __init__(self, headless):
         options = Options()
         if headless:
-            options.add_argument('--headless')  # Run in headless mode
-            options.add_argument('--disable-gpu')
-            options.add_argument('--window-size=1920,1080')  # Set window size to avoid layout issues
+            options.add_argument('--headless=new')
 
-        self.driver = webdriver.Chrome(options=options)
-    
+        service = Service(ChromeDriverManager().install())
+        self.driver = webdriver.Chrome(options=options, service=service)
+
+    def prompt_stars(self):
+        while True:
+            inp = input('How many stars (1-5)?')
+            if inp.isdigit():
+                n_stars = int(inp)
+                if 1 <= n_stars <= 5:
+                    break
+            print('Invalid stars. Please enter a value between 1-5.')
+        return n_stars
+
+    def prompt_comment(self):
+        while True:
+            comment = input('Enter comment: ')
+            if comment.strip():
+                break
+            print('Invalid comment. Please enter at least 1 character.')
+        return comment
+
+    def prompt_values(self):
+        n_stars = self.prompt_stars()
+        comment = self.prompt_comment()
+        return (n_stars, comment)
+
+    def read_links(self, filename):
+        with open(filename, 'r') as f:
+            return [line.strip() for line in f if line.strip()]
+
     def get_page(self, url):
         self.driver.get(url)
-        self.driver.implicitly_wait(2)
-        self.main()
 
-    def main(self):
-        table = self.driver.find_element(By.XPATH, '//table[@class="clp_sv_matrix_table"]')
-        questions = table.find_elements(By.XPATH, './/tr[@class="subquestion"]')
-        
+    def rate_stars(self, n_stars):
+        questions = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_all_elements_located((By.XPATH, '//table[@class="clp_sv_matrix_table"]//tr[@class="subquestion"]'))
+        )
+
         for question in questions:
-            choices = question.find_elements(By.XPATH, './/input[@type="radio"]')
-            choices[-2].click()
-        
+            stars = question.find_elements(By.XPATH, './/input[@type="radio"]')
+            stars[n_stars - 1].click()
+
+    def fill_comment(self, comment):
         # Switch to iframe
-        iframe = self.driver.find_element(By.XPATH, '//iframe')
+        iframe = WebDriverWait(self.driver, 5).until(
+            EC.presence_of_element_located((By.XPATH, '//iframe'))
+        )
         self.driver.switch_to.frame(iframe)
 
-        # Find textarea
-        textarea = self.driver.find_element(By.ID, 'tinymce')
-        time.sleep(1)
-        textarea.send_keys('ok')
+        # Type comment
+        self.driver.find_element(By.ID, 'tinymce').send_keys(comment)
 
         # Switch back
         self.driver.switch_to.default_content()
 
-        # Submit
-        submit = self.driver.find_element(By.ID, 'submit0')
-        submit.click()
+    def submit(self):
+        self.driver.find_element(By.ID, 'submit0').click()
+
+    def main(self):
+        urls = self.read_links('links.txt')
+
+        for url in urls:
+            try:
+                self.get_page(url)
+                n_stars, comment = self.prompt_values()
+                self.rate_stars(n_stars)
+                self.fill_comment(comment)
+                self.submit()
+            except Exception as e:
+                print(f'Error encountered for link "{url}": {e}')
+        self.driver.quit()
 
 if __name__ == '__main__':
-    url = input('URL: ')
-    scraper = Scraper(headless=False)
-    scraper.get_page(url)
+    evaluator = Evaluator(headless=False)
+    evaluator.main()
